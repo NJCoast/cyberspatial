22b1e9107c4735d0bef55c7e990ea6c309c83803
Charles F Vardeman II <charles.vardeman@gmail.com>
2017-05-10 10:04:52 -0400

    Refactoring names to njcoast

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 setup(
-    name="njdca",
+    name="cyberspatial",
     version="0.2",
     author="",
     author_email="",
-    description="njdca, based on GeoNode",
+    description="Cyberspatial framework for Resilience situational awareness, based on GeoNode",
     long_description=(read('README.rst')),
     # Full list of classifiers can be found at:
     # http://pypi.python.org/pypi?%3Aaction=list_classifiers
@@ -17,9 +17,9 @@ setup(
         'Development Status :: 1 - Planning',
     ],
     license="BSD",
-    keywords="njdca geonode django",
-    url='https://github.com/njdca/njdca',
-    packages=['njdca',],
+    keywords="geonode django",
+    url='https://github.com/njcoast/cyberspatial',
+    packages=['cyberspatial',],
     include_package_data=True,
     zip_safe=False,
     install_requires=[
