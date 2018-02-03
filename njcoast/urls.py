4809c53ffc4040db6c94ae04d9cb9dd311969e33
Caleb Reinking <calebreinking@hotmail.com>
2018-02-03 11:50:31 -0500

    deleting local njcoast folder to replace it with a submodule reference

--- njcoast/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import patterns, url
-from django.views.generic import TemplateView
-
-from geonode.urls import *
-
-urlpatterns = patterns('',
-    url(r'^/?$',TemplateView.as_view(template_name='site_index.html'), name='home'),
-    url(r'^about/$', TemplateView.as_view(template_name='site_about.html'), name='about'),
- ) + urlpatterns
